30441144967f3c60ac9cc7c3cc01cdccee46186b
audreybenetti <audrey.sestari@gmail.com>
2022-09-05 21:08:35 -0300

    Costumer microservice configuration.

---
+++ costumer-microservice/src/main/java/br/com/ada/CostumerApplication.java
@@ -0,0 +1,15 @@
+package br.com.ada;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class CostumerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CostumerApplication.class, args);
+    }
+
+}
